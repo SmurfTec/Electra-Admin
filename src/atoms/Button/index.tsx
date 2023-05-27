b636e6df481e75210d709a6de5003996efdaf2a4
noobcoder24 <zainnaeemk10@gmail.com>
2023-05-27 23:16:32 +0500

    add product

--- src/atoms/Button/index.tsx
+++ src/atoms/Button/index.tsx
@@ -1,9 +1,9 @@
 
 
-export const CustomButton = ({txt,classes,value,valueclasses,...props}:any) => {
+export const CustomButton = ({txt,classes,value,valueclasses,icon,...props}:any) => {
   return (
-     <div {...props} className={`w-[397px] h-[72px] overflow-hidden flex items-center justify-center bg-black text-[white] text-[16px] font-[500] rounded-[17px] cursor-pointer ${classes}`}>
-      
+     <div {...props} className={`w-[397px] h-[72px] gap-3 overflow-hidden flex items-center justify-center bg-black text-[white] text-[16px] font-[500] rounded-[17px] cursor-pointer ${classes}`}>
+      {icon && <i className="pi pi-search  "></i>}
       {txt}
       {value && <div className={`w-[23px] h-[23px] flex justify-center items-center text-[14px] rounded-[50px]  ${valueclasses}`}>
         {value}
--- src/pages/Products/AddProduct/index.tsx
+++ src/pages/Products/AddProduct/index.tsx
@@ -1,5 +1,11 @@
 import { Header, Variants } from "../../../components";
-import { InputTxt, CustomDropdown, CustomButton,UploadPicture } from "../../../atoms";
+import {
+  InputTxt,
+  CustomDropdown,
+  CustomButton,
+  UploadPicture,
+  FetchButton,
+} from "../../../atoms";
 export const AddProduct = () => {
   const VariantsArray = [
     {
@@ -92,13 +98,17 @@ export const AddProduct = () => {
         MainClasses="mt-[40px] !w-[80%]"
       />
       <div className="flex gap-4">
-        <CustomDropdown placeholder="Phone" mainclasses={"mt-10"} />
+        <CustomDropdown placeholder="Phone" mainclasses={"mt-10 "} />
         <InputTxt
           placeholder="Brand"
-          MainClasses="mt-[40px] !border-2 !border-[#FCFCFC] "
+          MainClasses="mt-[40px] !border !border-custom "
         />
-        <InputTxt placeholder="Model" MainClasses="mt-[40px] " />
+        <InputTxt
+          placeholder="Model"
+          MainClasses="mt-[40px] !border !border-custom"
+        />
       </div>
+      <textarea className=" bg-lightgray border border-custom mt-4 rounded-[8px] w-[75%] h-[142px] overflow-hidden pl-[21px] pr-[22px] focus:outline-none" />
       <div>
         <CustomButton
           txt={"Variants"}
@@ -129,11 +139,81 @@ export const AddProduct = () => {
         </div>
       </div>
       <div>
-      <CustomButton txt={"Pictures"} classes={"!w-[100px] !h-[40px] !mt-6 !rounded-[12px] !bg-[#EFEFEF] !text-[black]"}/>
-      <UploadPicture />
-      <CustomButton txt={"Technical Specifications"} classes={"!w-[220px] !h-[40px] !mt-6 !rounded-[12px] !bg-[#EFEFEF] !text-[black] !px-2 "}/>
-      <CustomButton txt={"Search Item"} classes={"!w-[220px] !h-[55px] !mt-6 !rounded-[12px] !bg-[#3C82D6] !text-[white] !px-2 "}/>
+        <CustomButton
+          txt={"Pictures"}
+          classes={
+            "!w-[100px] !h-[40px] !mt-6 !rounded-[12px] !bg-[#EFEFEF] !text-[black]"
+          }
+        />
+        <UploadPicture />
+        <CustomButton
+          txt={"Technical Specifications"}
+          classes={
+            "!w-[220px] !h-[40px] !mt-6 !rounded-[12px] !bg-[#EFEFEF] !text-[black] !px-2 "
+          }
+        />
+        <div className="flex gap-3 mt-5">
+          <FetchButton txt={"Fetch from Database"} />
+          <FetchButton txt={"Enter Manually"} />
+        </div>
+        <CustomButton
+          txt={"Search Item"}
+          classes={
+            "!w-[220px] !h-[55px] !mt-6 !rounded-[12px] !bg-[#3C82D6] !text-[white] !px-2 "
+          }
+          icon={true}
+        />
+        <div>
+          <p className="bg-lightgray p-4 w-[60%] rounded mt-5 border border-custom">
+            Technical Specifiactions for <b>iphone 14 pro max</b> was selected
+            via 3rd party. Click on search to replace
+          </p>
+        </div>
+        <div className="mb-5">
+          <p className="text-[black] font-extrabold bg-lightgray border-b-0 p-4 w-[60%] rounded mt-5 border border-custom">
+            Technical Specifications
+          </p>
 
+          <div className="border border-custom  w-[60%] pb-4">
+            <div className="ml-5">
+              <p className="text-[#656565] text-[12px] mt-4">RELEASE DATE</p>
+              <p className="font-medium"> 20 aug, 2022</p>
+            </div>
+            <div className="ml-5">
+              <p className="text-[#656565] text-[12px] mt-4">BLUETOOTH</p>
+              <p className="font-medium"> 5.0 </p>
+            </div>
+            <div className="ml-5">
+              <p className="text-[#656565] text-[12px] mt-4">BATTERY</p>
+              <p className="font-medium"> Up to 28 hours of video playback </p>
+            </div>
+            <div className="ml-5">
+              <p className="text-[#656565] text-[12px] mt-4">STORAGE</p>
+              <p className="font-medium"> 128 GB - 256GB - 512 GB </p>
+            </div>
+            <div className="ml-5">
+              <p className="text-[#656565] text-[12px] mt-4">CAMERA</p>
+              <p className="font-medium">
+                {" "}
+                Triple 12MP Ultra Wide, Wide, and Telephoto cameras with Night
+                mode, Deep Fusion, and ProRes video recording
+              </p>
+            </div>
+            <div className="ml-5">
+              <p className="text-[#656565] text-[12px] mt-4">cONNECTIVITY</p>
+              <li className="font-medium"> 5G capable </li>
+              <li className="font-medium"> Wi-Fi 6 (802.11ax) with MIMO</li>
+              <li className="font-medium"> Bluetooth 5.0 </li>
+            </div>
+            <div className="ml-5">
+              <p className="text-[#656565] text-[12px] mt-4"> SCREEN </p>
+              <p className="font-medium">
+                {" "}
+                6.1-inch and 6.7-inch Super Retina XDR display{" "}
+              </p>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
