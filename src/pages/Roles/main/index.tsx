1d8711afa8f1fabfc7f71ac0940f6934d7e48359
noobcoder24 <zainnaeemk10@gmail.com>
2023-05-28 16:25:23 +0500

    Headers for roles

--- src/pages/Roles/main/index.tsx
+++ src/pages/Roles/main/index.tsx
@@ -1,7 +1,12 @@
 import React from 'react'
-
+import { Header } from '../../../components'
 export const Roles = () => {
   return (
-    <div>Roles</div>
+    <div>
+      <Header placeholder='Search Admins'
+       typeSearch={true}
+       chooseFilter={true}
+       UserBox={true}/>
+    </div>
   )
 }
