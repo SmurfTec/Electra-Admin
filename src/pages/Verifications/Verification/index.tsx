d1ce8b672ea4ac7ae5f3b9cffc52c26bd913481d
Shehryar_Kiyani <shehryar@xenex-media.com.au>
2023-06-03 14:49:26 +0500

    verification txt

--- src/pages/Verifications/Verification/index.tsx
+++ src/pages/Verifications/Verification/index.tsx
@@ -1,6 +1,8 @@
-import React from 'react'
+import React,{useState} from 'react'
 import { Header } from '../../../components'
+import { InputTxt } from '../../../atoms'
 export const Verification = () => {
+  const[OrderTrack,setOrderTrack]=useState('')
   return (
     <div>
         <Header
@@ -8,6 +10,16 @@ export const Verification = () => {
          typeSearch={true}
          UserBox={true}
        />
+       <div className='flex mt-[37px]'>
+       <InputTxt
+        placeholder="Enter Order/Tracking Number"
+        MainClasses="!bg-[#FCFCFC] pointer  !rounded-[8px] border !border-inputBorder !w-[300px] !h-[59px] "
+        iconLeft={true}
+       
+        value={OrderTrack}
+        onChange={(e: any) => setOrderTrack(e.target.value)}
+      />
+       </div>
     </div>
   )
 }
