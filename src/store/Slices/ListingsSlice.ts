996e1e0d9e47510c80cdfc79f3be43e2d357824e
noobcoder24 <zainnaeemk10@gmail.com>
2023-06-23 20:05:32 +0500

    Product voew table

--- src/store/Slices/ListingsSlice.ts
+++ src/store/Slices/ListingsSlice.ts
@@ -3,9 +3,18 @@ import url from "../../config/index";
 export const getAllListings = async () => {
   try {
     let response: any = await url.get("/listings");
+    console.log(response);
+    return response;
+  } catch (e) {
+    return e;
+  }
+};
+export const getListingById = async (id: any) => {
+  try {
+    let response: any = await url.get(`/listings/${id}`);
     console.log(response)
     return response;
   } catch (e) {
-    return e
+    return e;
   }
 };
